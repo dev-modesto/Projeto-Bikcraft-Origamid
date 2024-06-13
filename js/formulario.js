beb021ff7925eac02aa6e93f77418738bbad6942
dev-modesto <gabriel_26@outlook.com.br>
2024-06-12 21:01:58 -0300

    feat: form javascript

---
+++ js/formulario.js
@@ -0,0 +1,29 @@
+const formulario = document.querySelector('form');
+
+console.log(formulario);
+
+function formularioEnviado(resposta){
+    if(resposta.ok){
+        formulario.innerHTML = "<p  class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style=' color: #317a00;'>Mensagem enviada</span>, em breve entraremos em contato.</p>"
+    }else {
+        formulario.innerHTML = "<p  class='font-2-l' style='grid-column: 1/-1; padding: 1rem; border-radius: 4px; background: #f7f7f7;'><span style='color: #e00000;'>Erro no envio</span>, você pode enviar diretamente para o nosso email em: contato@bikcraft-devmodesto.com.br.</p>"
+    }
+}
+
+function enviarFormulario(event) {
+    event.preventDefault();
+    const botao = document.querySelector('form button');
+    botao.disabled = true;
+    botao.innerText = "Enviando...";
+
+    const data = new FormData(formulario);
+    // console.log(data.get('email'));
+
+    fetch('./enviar.php', {
+        method: 'POST',
+        body: data
+    }).then(formularioEnviado);
+
+}
+
+formulario.addEventListener('submit', enviarFormulario);
